96b07168c3e9d04889b79a080568d29f32b6676d
abdumalik2014 <abdumalik2014@gmail.com>
2019-07-06 12:33:11 +0500

    Fixed
    fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ module.exports = function warmup(temperature) {
   if (temperature === -20) {
     return -4;
   }
-  if (temperature === 0) {
-    return 32;
-  }
+    if (temperature === 0) {
+        return 32;
+    }
+    else
+        return 1.8 * temperature + 32;
 };
